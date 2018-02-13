f80422d3f4a7b6f91ec59e24cdb0411396e60475
Lassiter Gregg <lassitergregg@gmail.com>
2018-02-12 19:29:22 -0600

    2487

---
+++ spec/controllers/welcome_controller_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe WelcomeController, type: :controller do
+  describe "GET index" do
+    it "renders the index template" do
+  # #2
+      get :index
+  # #3
+      expect(response).to render_template("index")
+    end
+  end
+
+  describe "GET about" do
+    it "renders the about template" do
+      get :about
+      expect(response).to render_template("about")
+    end
+  end
+
+  describe "GET contact" do
+    it "renders the contact template" do
+      get :contact
+      expect(response).to render_template("contact")
+    end
+  end
+
+
+
+end
