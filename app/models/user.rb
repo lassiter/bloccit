aba99ce4f3fcf6a3a694cb13ecaf47246611f6d8
Lassiter Gregg <lassitergregg@gmail.com>
2018-03-14 17:35:57 -0500

    correction for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,28 +1,24 @@
  class User < ApplicationRecord
- # #2
-   before_save { self.email = email.downcase if email.present? }
-   before_save { 
-    full_name = self.name.split(" ") if name.present?
-    if full_name.length > 1
-      full_name.each_with_index {|n,i| p full_name[i] = n.capitalize}  if full_name.present?
-    end
-    self.name = full_name.join(" ") if full_name.present?
-    # self.name = self.name.split(" ").map do |name|
-    #   name.capitalize 
-    # end.join(" ")
-   }
-
- # #3
-   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
- # #4
+  before_save :format_name, :format_email
+  validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
   validates :password, length: { minimum: 6 }, allow_blank: true
- # #5
    validates :email,
              presence: true,
              uniqueness: { case_sensitive: false },
              length: { minimum: 3, maximum: 254 }
 
- # #6
-   has_secure_password
+  has_secure_password
+  private
+    def format_name
+      if name.present?
+        full_name = self.name.split(" ")
+        full_name.each_with_index {|n,i| full_name[i] = n.capitalize}
+        self.name = full_name.join(" ")
+      end
+    end
+
+    def format_email
+      self.email = email.downcase if email.present?
+    end
  end
