ce2c577f5735adc2a7aa3ee03c9cd4ca2c1df3c8
Lassiter Gregg <lassitergregg@gmail.com>
2018-03-14 20:10:38 -0500

    added default scopes and orders for post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,9 +4,17 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   default_scope { order('created_at DESC') }
 
+
    validates :title, length: { minimum: 5 }, presence: true
    validates :body, length: { minimum: 20 }, presence: true
    validates :topic, presence: true
    validates :user, presence: true
 
+
+  def self.ordered_by_title
+    order('title ASC')
+  end
+  def self.ordered_by_reverse_created_at
+    order('created_at ASC')
+  end
 end
